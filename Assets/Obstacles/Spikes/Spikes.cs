f3460a9d349b16bede7b7bd4a489aa4c060a04d2
Pesheyden <138362492+Pesheyden@users.noreply.github.com>
2024-09-04 16:42:42 +0200

    Level 4

--- Assets/Obstacles/Spikes/Spikes.cs
+++ Assets/Obstacles/Spikes/Spikes.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.CompareTag("Player"))
         {
