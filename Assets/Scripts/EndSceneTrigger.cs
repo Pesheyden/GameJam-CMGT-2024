94567dcb673b9e26dc1004a2b744825decdae14f
Pesheyden <138362492+Pesheyden@users.noreply.github.com>
2024-09-05 10:48:24 +0200

    Level 5

--- Assets/Scripts/EndSceneTrigger.cs
+++ Assets/Scripts/EndSceneTrigger.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class EndSceneTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject _anim;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (LayerMask.LayerToName(other.gameObject.layer) == "Ghost")
         {
-            //Todo: start end animation
+            Camera.main.gameObject.SetActive(false);
+            PlayerController.Instance.ChangePlayerStatus(false);
+            _anim.SetActive(true);
+            _anim.GetComponent<Animation>().Play();
+            Invoke("OpenMenu", 60f);
         }
+    }
+
+    private void OpenMenu()
+    {
+        GameManager.Instance.SetScene("MainMenu");
     }
 }
